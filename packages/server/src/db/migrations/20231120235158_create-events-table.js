5993bc8a2f35e52210c060a5674434fa8c031e3d
Rodrigo Saling <rodrigosaling@gmail.com>
2024-02-13 20:08:39 -0300

    removes the groupID column from the event table

--- packages/server/src/db/migrations/20231120235158_create-events-table.js
+++ packages/server/src/db/migrations/20231120235158_create-events-table.js
@@ -1,6 +1,5 @@
 /* eslint-disable import/extensions */
 import { EVENTS_TABLE_NAME } from '../constants/events.constants.js';
-import { GROUPS_TABLE_NAME } from '../constants/group.constants.js';
 import { PEOPLE_TABLE_NAME } from '../constants/people.constants.js';
 
 export const up = (knex) =>
@@ -10,12 +9,10 @@ export const up = (knex) =>
     // "UI id" to be used on UI platforms instead of the regular id
     table.string('uiid', 11).notNullable().unique();
     table.integer('ownerId').unsigned().notNullable();
-    table.integer('groupId').unsigned().notNullable();
     table.boolean('isDeleted').notNullable().defaultTo(false);
     table.timestamps(false, true, true);
 
     table.foreign('ownerId').references('id').inTable(PEOPLE_TABLE_NAME);
-    table.foreign('groupId').references('id').inTable(GROUPS_TABLE_NAME);
   });
 
 export const down = (knex) => knex.schema.dropTable(EVENTS_TABLE_NAME);
